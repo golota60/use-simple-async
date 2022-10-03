daa349a25b02d89db8dff5c69e774fd6875ab3b1
Szymon Wiszczuk <golota60@gmail.com>
2022-10-03 21:00:03 +0200

    fix(types): enable strict mode

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,13 @@ interface FuncMeta {
  * A hook that allows to execute async functions with some helpful metadata.
  *
  * If the function passed as argument is redeclared on every render, it has to be wrapped with a useCallback
- * @param {function} asyncFunc - Async function to be executed
- * @param {Object} options
- *
- * @returns [data, {loading, error}]
  */
 const useSimpleAsync = <T, V>(
-  asyncFunc: (variables: V) => Promise<T>,
-  options?: { skip?: boolean; variables?: V }
+  asyncFunc: (variables?: V) => Promise<T>,
+  options: { skip?: boolean; variables?: V } = {}
 ): [T | undefined, FuncMeta] => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<T | undefined>();
+  const [data, setData] = useState<T>();
   const [error, setError] = useState<unknown>();
 
   const { skip = false, variables = {} } = options;
